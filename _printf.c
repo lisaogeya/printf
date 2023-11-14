ad11189ecc72ba63d607fc6d0f762d73db708495
lisaogeya <lisaogeya.l@gmail.com>
2023-11-14 10:44:04 -0800

    Printc

--- _printf.c
+++ _printf.c
@@ -12,18 +12,4 @@ int _printf(const char *format, ...)
 int total = 0;
 va_list the_arg_list;
 va_start(the_arg_list, format);
-while (*format)
-{
-if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's' || *(format + 1) == '%' || *(format + 1) == 'd' || *(format + 1) == 'i'))
-{
-format += 2; /*move to the next character*/
-}
-else
-{
-total += _putchar(*format);
-format++;
-}
-}
-va_end(the_arg_list);
-return (total);
-}
+
