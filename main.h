a11e4436567458c8e8a1c0e51809cfca3fb30df9
lisaogeya <lisaogeya.l@gmail.com>
2023-11-14 10:24:42 -0800

    header file

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 int _printf(const char *format, ...);
 int print_integer(va_list arg_toprint);
 int print_decimal(va_list arg_toprint);
