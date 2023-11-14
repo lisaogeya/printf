fd929b4cdda8e152d295a3835091736b95815d3d
Akintope <akinmoro@gmail.com>
2023-11-14 09:42:24 -0800

    Print int

--- _printf.c
+++ _printf.c
@@ -9,24 +9,21 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-
-    va_start(args, format);
-
-    while (*format)
-    {
-        if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's' || *(format + 1) == '%' || *(format + 1) == 'd' || *(format + 1) == 'i'))
-        {
-            format += 2; /*move to the next character*/
-        }
-        else
-        {
-            count += _putchar(*format);
-            format++;
-        }
-    }
-
-    va_end(args);
-    return (count);
+int count = 0;
+va_list args;
+va_start(args, format);
+while (*format)
+{
+if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's' || *(format + 1) == '%' || *(format + 1) == 'd' || *(format + 1) == 'i'))
+{
+format += 2; /*move to the next character*/
+}
+else
+{
+count += _putchar(*format);
+format++;
+}
+}
+va_end(args);
+return (count);
 }
