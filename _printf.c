7631c27e7ec05c9bce87f0aa659afebbdafa19ec
lisaogeya <lisaogeya.l@gmail.com>
2023-11-14 12:05:39 -0800

    printtf

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdarg.h>
 
 /**
  * _printf - Prints output according to a format
@@ -9,7 +8,53 @@
  */
 int _printf(const char *format, ...)
 {
+char *string;
 int total = 0;
+char *c;
+int length;
 va_list the_arg_list;
 va_start(the_arg_list, format);
+if (format == NULL)
+{
+return (-1);
+}
+while (*format != '\0')
+{
+if (*format != '%')
+{
+write (STDOUT_FILENO, format, 1);
+total++;
+}
+else
+{
+switch (*++format)
+{
+case 'c':
+*c = va_arg(the_arg_list, int);
+write(STDOUT_FILENO, &c, 1);
+total++;
+break;
+case 's':
+string = va_arg(the_arg_list, char *);
+length = 0;
+for (; *string != '\0'; ++string)
+{
+length++;
+}
+write(STDOUT_FILENO, string, length);
+total += length;
+break;
+
+default:
+putchar('%');
+write(STDOUT_FILENO, format, 1);
+total += 2;
+break;
+}
+}
+format++;
+}
+va_end(the_arg_list);
+return (total);
+}
 
