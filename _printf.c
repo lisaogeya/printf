fa4e8c30d619b9ba92ebd0e1df2a3a6dbc79b8c3
Akintope <akinmoro@gmail.com>
2023-11-14 07:41:58 -0800

    Print int

--- _printf.c
+++ _printf.c
@@ -5,27 +5,41 @@
  * @format: Format string with optional placeholders.
  * Return: Number of characters printed.
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-convert p[] = {
-{"%i", print_i}, {"%d", print_d}, {"%r", print_revs},
-va_list args;
-int k = 0, length = 0;
-va_start(args, format);
-while (format[k] != '\0')
-{
-if (format[k] == '%' && (format[k + 1] == 'd' || format[k + 1] == 'i'))
-{
-length += print_integer(args);
-k += 2;  /* Move past the format specifier */
-}
-else
-{
-_putchar(format[k]);
-length++;
-k++;
-}
-}
-va_end(args);
-return (length);
+    convert p[] = {
+        {"%i", print_integer}, {"%d", print_integer}, {"%r", print_revs},
+    };
+
+    va_list args;
+    int k = 0, length = 0;
+
+    va_start(args, format);
+
+    while (format[k] != '\0')
+    {
+        if (format[k] == '%' && (format[k + 1] == 'd' || format[k + 1] == 'i'))
+        {
+            int j = 0;
+            while (j < sizeof(p) / sizeof(p[0]))
+            {
+                if (strcmp(format + k, p[j].ph) == 0)
+                {
+                    length += p[j].function(args);
+                    break;
+                }
+                j++;
+            }
+            k += 2; /* Move past the format specifier */
+        }
+        else
+        {
+            _putchar(format[k]);
+            length++;
+            k++;
+        }
+    }
+
+    va_end(args);
+    return (length);
 }
