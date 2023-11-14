2080ea5e2991015dd685322f652ecb35aeb9aba7
Akintope <akinmoro@gmail.com>
2023-11-14 05:29:36 -0800

    Printing integers

---
+++ 0x11-printf_project/_printf.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* _printf - Custom printf function for %d and %i format specifiers.
+* @format: Format string with optional placeholders.
+* Return: Number of characters printed.
+*/
+int _printf(const char * const format, ...)
+{
+va_list args;
+int k = 0, length = 0;
+
+va_start(args, format);
+
+while (format[k] != '\0')
+{
+if (format[k] == '%' && (format[k + 1] == 'd' || format[k + 1] == 'k'))
+{
+length += print_integer(args);
+k += 2;  /* Move past the format specifier*/
+}
+else
+{
+_putchar(format[k]);
+length++;
+k++;
+}
+}
+va_end(args);
+return (length);
+}
