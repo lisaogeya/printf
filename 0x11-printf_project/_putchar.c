a20eabf96a87b38a8eecec9524b847092872a3f9
Akintope <akinmoro@gmail.com>
2023-11-14 03:45:01 -0800

    Print integer

--- 0x11-printf_project/_putchar.c
+++ 0x11-printf_project/_putchar.c
@@ -3,8 +3,9 @@
 *_putchar - writes character to standard output
 *@c: Character to be printed
 *Return: 1 if successful
-*Return: -1 if there is error
 */
 int _putchar(char c);
 int print_integer(va_list arg_toptint);
-#endif
+{
+return (write(1, &c, 1));
+}
